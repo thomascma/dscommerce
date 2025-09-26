95b85ea89395ce758e3732039e35dc33c34e32f5
Tomás Rocha <tomascma@hotmail.com>
2025-09-26 10:36:25 -0300

    Adiciona o findAll paginado no productService

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,10 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product entity = repository.findById(id).get();
         return new ProductDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return repository.findAll(pageable).map(ProductDTO::new);
     }
 }
