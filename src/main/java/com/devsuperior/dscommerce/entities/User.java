a79c1e943b32197c8a160ee912ace02cb39b2467
Tomás Rocha <tomascma@hotmail.com>
2025-10-09 10:31:00 -0300

    Adiciona dependencias de segurança e entidades User/Role

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,8 +3,10 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
@@ -14,16 +16,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    
+
     @Column(unique = true)
     private String email;
-    
+
     private String phone;
     private LocalDate birthDate;
     private String password;
-    
+
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+    
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
@@ -87,6 +95,10 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 
     @Override
